1f34b5d600905ba2c5165a721770b43ad3c7950e
Jinho Kim <wlsghrla94@gmail.com>
2022-06-24 09:45:26 +0900

    .

--- 07.vue/vue-learning/chap05_router/src/main.js
+++ 07.vue/vue-learning/chap05_router/src/main.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // npm i vue-router@3
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
-
-
+  render: (h) => h(App),
+}).$mount("#app");
